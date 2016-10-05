ab1a31e4e5d809f19e2f0d2d5d90687f39ec40b0
Binur Konarbay <binur95@gmail.com>
2016-10-05 16:57:58 +0600

    Update Example app

--- Example/App.js
+++ Example/App.js
@@ -2,48 +2,81 @@
  * @flow
  */
 
-import { View, StyleSheet } from 'react-native';
-import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, { Component } from 'react';
 import ImageCarousel from 'react-native-image-carousel';
 
-const slides = [
-  {
-    imageUrl: 'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/66807.max-620x600.jpg',
-    imageHeight: 150,
-    caption: 'Clown',
-  },
-  {
-    imageUrl: 'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/67003.max-620x600.jpg',
-    imageHeight: 200,
-    caption: 'I need a new costume',
-  },
-  {
-    imageUrl: 'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/51681.max-620x600.jpg',
-    imageHeight: 150,
-    caption: 'Joker',
-  },
-  {
-    imageUrl: 'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/66812.max-620x600.jpg',
-    imageHeight: 150,
-    caption: 'I\'m Batman',
-  },
-  {
-    imageUrl: 'https://myanimelist.cdn-dena.com/s/common/uploaded_files/1438960604-925d1997518b66f8508c749f36810793.jpeg',
-    imageHeight: 150,
-    caption: 'I\'m Saitama!',
-  },
+const urls = [
+  'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/66807.max-620x600.jpg',
+  'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/67003.max-620x600.jpg',
+  'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/51681.max-620x600.jpg',
+  'https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/66812.max-620x600.jpg',
+  'https://myanimelist.cdn-dena.com/s/common/uploaded_files/1438960604-925d1997518b66f8508c749f36810793.jpeg',
 ];
 
-const App = () => (
-  <View style={styles.container}>
-    <ImageCarousel
-      heading={'Stills'}
-      headingStyle={styles.heading}
-      captionStyle={styles.caption}
-      slides={slides}
-    />
-  </View>
-);
+export default class App extends Component<any, any, any> {
+  _imageCarousel: ImageCarousel;
+
+  componentWillMount() {
+    (this: any)._renderHeader = this._renderHeader.bind(this);
+  }
+
+  _renderHeader(): ReactElement<any> {
+    return (
+      <TouchableWithoutFeedback onPress={this._imageCarousel.close}>
+        <View>
+          <Text style={styles.closeText}>Exit</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+
+  _renderFooter(): ReactElement<any> {
+    return (
+      <Text style={styles.footerText}>Footer!</Text>
+    );
+  }
+
+  _renderContent(idx: number): ReactElement<any> {
+    return (
+      <Image
+        style={styles.container}
+        source={{ uri: urls[idx] }}
+        resizeMode={'contain'}
+      />
+    );
+  }
+
+  render(): ReactElement<any> {
+    return (
+      <View style={styles.container}>
+        <ImageCarousel
+          ref={(imageCarousel: ImageCarousel) => {
+            this._imageCarousel = imageCarousel;
+          }}
+          renderContent={this._renderContent}
+          renderHeader={this._renderHeader}
+          renderFooter={this._renderFooter}
+        >
+          {urls.map((url: string): ReactElement<any> => (
+            <Image
+              key={url}
+              style={styles.image}
+              source={{ uri: url, height: 100 }}
+              resizeMode={'contain'}
+            />
+          ))}
+        </ImageCarousel>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -51,13 +84,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  heading: {
-    padding: 15,
-    fontSize: 28,
+  closeText: {
+    color: 'white',
+    textAlign: 'right',
+    padding: 10,
   },
-  caption: {
-    fontSize: 20,
+  footerText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  image: {
+    marginRight: 2,
+    width: 100,
+    height: 100,
   },
 });
-
-export default App;
