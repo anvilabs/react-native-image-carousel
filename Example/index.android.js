9c28d68b364edb6a76f61cdeaebd8a78fe8c05e8
Binur Konarbay <binur95@gmail.com>
2017-06-26 17:39:23 +0600

    feat(Example): use local rn-image-carousel module

--- Example/index.android.js
+++ Example/index.android.js
@@ -4,4 +4,4 @@ import {AppRegistry} from 'react-native';
 
 import App from './App';
 
-AppRegistry.registerComponent('Example', (): App => App);
+AppRegistry.registerComponent('Example', () => App);
