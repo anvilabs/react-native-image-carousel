88f3c43f5bfd56a60eed2bc8312fa17554cc7333
Binur Konarbay <binur95@gmail.com>
2017-06-27 19:27:53 +0600

    feat: simplify build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,14 @@
 /* @flow */
 
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import flow from 'rollup-plugin-flow';
-import resolve from 'rollup-plugin-node-resolve';
 
 const config = {
   entry: 'src/index.js',
   dest: 'dist/index.js',
-  format: 'es',
-  external: ['react', 'react-native'],
-  plugins: [
-    flow(),
-    resolve({
-      customResolveOptions: {
-        moduleDirectory: 'node_modules',
-      },
-    }),
-    commonjs(),
-    babel({exclude: 'node_modules/**'}),
-  ],
+  format: 'cjs',
+  external: ['react', 'react-native', 'react-swipeable-views-native'],
+  plugins: [flow(), babel({exclude: 'node_modules/**'})],
 };
 
 export default config;
