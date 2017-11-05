e7d00ca50ac77c13b20b73d1158f979468111b74
Binur Konarbay <binur95@gmail.com>
2017-11-05 18:18:43 +0600

    fix: fix Example crash

--- Example/rn-cli.config.js
+++ Example/rn-cli.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-// As the metro bundler does not support linking correctly, we add additional
-// search path queries to all modules.
 // See https://github.com/facebook/metro-bundler/issues/68
 const extraNodeModulesGetter = {
   get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
